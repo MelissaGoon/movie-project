387c0ed7d8af710feb981f160d4843e25df881c3
Melissa Goon <melissagoon13743@gmail.com>
2025-09-26 23:28:14 -0700

    Finished about page

--- src/pages/PageAbout.jsx
+++ src/pages/PageAbout.jsx
@@ -1,7 +1,30 @@
+import { ASSETS_FOLDER_PATH } from "../globals/global-variables";
+import styles from '../styles/modules/About.module.css';
+import heroStyles from '../styles/modules/SingleMovie.module.css';
 
 const PageAbout = () => {
     return (
-        <main id="site-main"><div>PageAbout</div></main>
+        <main id="site-main">
+            <section className={heroStyles.hero}>
+
+                <img src={`${ASSETS_FOLDER_PATH}placeholder-background.svg`} alt="A repeating pattern of a cartoon ghost with sunglasses on." />
+
+
+                <div className={heroStyles.hero_text}>
+                    <h1 className={styles.title}>👻 About Us 👻</h1>
+
+                </div>
+            </section>
+
+            <section className={styles.info}>
+                <h2>What is ghostlyDB?</h2>
+                <p>GhostlyDB is a simple, clean movie database powered by The Movie Database (TMDb) API. Browse, search, and discover films from every genre, guided by our friendly ghost mascot. Built as a passion project, GhostlyDB offers a lightweight, easy-to-use space for movie lovers to explore cinema both classic and new.</p>
+                <img className={styles.ghost_logo} src={`${ASSETS_FOLDER_PATH}logo-ghost.svg`} alt="The ghostlyDB Logo" />
+                <p>This product uses the TMDB API but is not endorsed or certified by TMDB.</p>
+                <img className={styles.tmdb_logo} src={`${ASSETS_FOLDER_PATH}tmdb_logo.svg`} alt="The TMDB Logo" />
+            </section>
+
+        </main>
     )
 }
 
